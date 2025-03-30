26b2f254e5648de99a85610c2acfba8d387a04f5
gourav sharma <gourav.sharma.glamat2932@gmail.com>
2025-03-31 01:59:23 +0530

    www

--- app/layout.js
+++ app/layout.js
@@ -31,15 +31,15 @@ export default function RootLayout({ children }) {
                 <meta property='og:description' content='Explore the rich heritage of Braj with Braj-Darshan | Your AI-powered guide for temples, history, culture, and travel in Mathura, Vrindavan, and beyond!' />
                 {/* Icon : Peacock icons created by Freepik - Flaticon 
                     link : https://www.flaticon.com/free-icons/peacock */}
-                <meta property='og:image' content='https://www.braj-darshan.vercel.app/favicon.png' />
-                <meta property='og:url' content='https://www.braj-darshan.vercel.app' />
+                <meta property='og:image' content='https://braj-darshan.vercel.app/favicon.png' />
+                <meta property='og:url' content='https://braj-darshan.vercel.app' />
                 <meta property="og:site_name" content="Braj-Darshan" />   
 
                 {/* Twitter */}
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:title" content="Braj-Darshan | Your AI guide to Braj" />
                 <meta name="twitter:description" content="Explore the rich heritage of Braj with Braj-Darshan | Your AI-powered guide for temples, history, culture, and travel in Mathura, Vrindavan, and beyond!" />
-                <meta name="twitter:image" content="https://www.braj-darshan.vercel.app/favicon.png" />
+                <meta name="twitter:image" content="https://braj-darshan.vercel.app/favicon.png" />
                 <meta name="twitter:site" content="@braj-darshan" />
             </head>
             <body>
